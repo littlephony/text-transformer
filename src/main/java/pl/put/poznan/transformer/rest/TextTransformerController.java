e7f7b1f01b857fe992a49d6391dbfd0e44351fc3
popuskowski <aaakulchyk@gmail.com>
2023-05-20 15:12:25 +0200

    Return response in JSON format

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,47 +1,41 @@
 package pl.put.poznan.transformer.rest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.Transformer;
-import pl.put.poznan.transformer.logic.TextTransformerErector;
 
 import java.util.Arrays;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import pl.put.poznan.transformer.logic.Transformer;
+import pl.put.poznan.transformer.logic.TextTransformerErector;
+
 
 @RestController
-@RequestMapping("/{text}")
+@RequestMapping("/")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+    public ResponseEntity<JsonNode> get(
+            @RequestParam(value="text") String text,
+            @RequestParam(value="transforms", defaultValue="upper") String[] transforms
+    ) throws JsonProcessingException {
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug(String.format("[Request] text = %s", text));
+        logger.debug(String.format("[Request] transforms = %s", Arrays.toString(transforms)));
 
-        // perform the transformation, you should run your logic here, below is just a silly example
         Transformer transformer = TextTransformerErector.erectTransformer(transforms);
-        return transformer.transform(text);
+        String result = transformer.transform(text);
+
+        logger.debug(String.format("[Response] result = %s", result));
+
+        JsonNode json = mapper.readTree(String.format("{\"result\": \"%s\"}", result));
+        return ResponseEntity.ok(json);
     }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        Transformer transformer = TextTransformerErector.erectTransformer(transforms);
-        return transformer.transform(text);
-    }
-
-
-
 }
-
-
